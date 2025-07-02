79b854025aa1e70d24894ba64df276d0f36a2eff
WFTGF2024 <giaogiaozhu@163.com>
2025-07-02 14:08:56 +0800

    template

---
+++ src/test/java/com/example/sensor_spring/SensorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensor_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
